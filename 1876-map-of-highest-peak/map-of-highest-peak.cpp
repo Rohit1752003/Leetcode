24cce12d4781c8f2ee755cb6904b35b8eaccec99
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-07-18 12:23:36 +0530

    Time: 98 ms (22.21%) | Memory: 135.8 MB (9.99%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,65 @@
+class Solution {
+    private:
+     vector<vector<int>> BFS(vector<vector<int>>& mat,
+                            vector<vector<int>>& vis,
+                            vector<vector<int>>& ans) {
+
+        queue<pair<pair<int, int>, int>> q;
+
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        // Push all 0's into queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (mat[i][j] == 1) {
+                    vis[i][j] = 1;
+                    q.push({{i, j}, 0});
+                } else {
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        while (!q.empty()) {
+
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+
+            q.pop();
+
+            ans[row][col] = steps;
+
+            for (int i = 0; i < 4; i++) {
+
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if (nrow >= 0 && nrow < n &&
+                    ncol >= 0 && ncol < m &&
+                    !vis[nrow][ncol]) {
+
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, steps + 1});
+                }
+            }
+        }
+
+        return ans;
+    }
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+         int n = isWater.size();
+        int m = isWater[0].size();
+
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        return BFS(isWater, vis, ans);
+    }
+};
