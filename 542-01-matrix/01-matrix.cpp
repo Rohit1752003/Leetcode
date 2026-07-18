d364c302ef86c1af69cbf61509c597ae6d917938
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-07-18 12:16:18 +0530

    Time: 12 ms (81.65%) | Memory: 38.7 MB (38.27%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 private:
     vector<vector<int>> BFS(vector<vector<int>>& mat,
                             vector<vector<int>>& vis,
@@ -63,5 +63,78 @@ public:
         vector<vector<int>> vis(n, vector<int>(m, 0));
 
         return BFS(mat, vis, ans);
+    }
+};
+*/
+
+//  OPtimize version  no vis array
+class Solution {
+private:
+    vector<vector<int>> BFS(vector<vector<int>>& mat,
+                            vector<vector<int>>& ans) {
+
+        int n = mat.size();
+        int m = mat[0].size();
+
+        queue<pair<pair<int,int>,int>> q;
+
+        int delRow[] = {-1,0,1,0};
+        int delCol[] = {0,1,0,-1};
+
+        // Push all 0's
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<m;j++){
+
+                if(mat[i][j]==0){
+
+                    q.push({{i,j},0});
+                    ans[i][j]=0;
+
+                }
+
+            }
+
+        }
+
+        while(!q.empty()){
+
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int steps=q.front().second;
+
+            q.pop();
+
+            for(int k=0;k<4;k++){
+
+                int nrow=row+delRow[k];
+                int ncol=col+delCol[k];
+
+                if(nrow>=0 && nrow<n &&
+                   ncol>=0 && ncol<m &&
+                   ans[nrow][ncol]==-1){
+
+                    ans[nrow][ncol]=steps+1;
+
+                    q.push({{nrow,ncol},steps+1});
+
+                }
+
+            }
+
+        }
+
+        return ans;
+    }
+
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
+        int n=mat.size();
+        int m=mat[0].size();
+
+        vector<vector<int>> ans(n,vector<int>(m,-1));
+
+        return BFS(mat,ans);
     }
 };
