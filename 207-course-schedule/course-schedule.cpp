4a09c70fa1707ddedeb9e8672e22f09aba6eb473
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-07-21 16:01:29 +0530

    Time: 3 ms (75.09%) | Memory: 19.1 MB (81.42%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -34,7 +34,7 @@ public:
            vector<vector<int>> adj(numCourses);
 
     for(auto &it : prerequisites) {
-        adj[it[0]].push_back(it[1]);
+        adj[it[1]].push_back(it[0]);
     }
 
     vector<int> indegree(numCourses, 0);
