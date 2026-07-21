48d736ae2ec0c420ac3e1be04c724697b5fc7675
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-07-21 15:59:32 +0530

    Time: 12 ms (14.36%) | Memory: 19.2 MB (81.42%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,50 @@
+class Solution {
+     private:
+   bool BFS(int V, vector<vector<int>>& adj, vector<int>& indegree) {
+
+    queue<int> q;
+
+    for(int i = 0; i < V; i++) {
+        if(indegree[i] == 0)
+            q.push(i);
+    }
+
+    int cnt = 0;
+
+    while(!q.empty()) {
+
+        int node = q.front();
+        q.pop();
+
+        cnt++;
+
+        for(auto it : adj[node]) {
+
+            indegree[it]--;
+
+            if(indegree[it] == 0)
+                q.push(it);
+        }
+    }
+
+    return cnt == V;
+} 
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+           vector<vector<int>> adj(numCourses);
+
+    for(auto &it : prerequisites) {
+        adj[it[0]].push_back(it[1]);
+    }
+
+    vector<int> indegree(numCourses, 0);
+
+    for(int i = 0; i < numCourses; i++) {
+        for(auto it : adj[i]) {
+            indegree[it]++;
+        }
+    }
+
+    return BFS(numCourses, adj, indegree);
+    }
+};
