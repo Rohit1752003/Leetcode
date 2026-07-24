3342673594943f7531f935255d9bc2a01f57b3f5
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-07-24 13:09:32 +0530

    Time: 46 ms (88.02%) | Memory: 21.3 MB (53.26%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -5,6 +5,7 @@ public:
         queue<pair<string,int>>q;
         q.push({beginWord ,1});
         st.erase(beginWord);
+        if(st.find(endWord)==st.end())return 0;
         while(!q.empty()){
             string node = q.front().first;
             int steps = q.front().second;
@@ -12,7 +13,7 @@ public:
             if(node==endWord)return steps;
             for(int i =0 ; i < node.size();i++){
                 char og = node[i];
-                for(char ch = 'a ' ; ch <= 'z';ch++ ){
+                for(char ch = 'a' ; ch <= 'z';ch++ ){
                     node[i]= ch;
                     if(st.find(node) != st.end()){
                         q.push({node,steps+1});
