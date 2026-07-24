fb4757d664a66c7695f8a1214d00f6578fcb9ddf
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-07-24 15:29:14 +0530

    Time: 15 ms (46.71%) | Memory: 14.3 MB (15.40%) - LeetSync

---
+++ 126-word-ladder-ii/word-ladder-ii.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    unordered_map<string, vector<string>> parent;
+    unordered_map<string, int> level;
+
+    void dfs(string word, string beginWord, vector<string>& path) {
+
+        if (word == beginWord) {
+            path.push_back(beginWord);
+
+            vector<string> temp = path;
+            reverse(temp.begin(), temp.end());
+
+            ans.push_back(temp);
+
+            path.pop_back();
+            return;
+        }
+
+        path.push_back(word);
+
+        for (auto &par : parent[word]) {
+            dfs(par, beginWord, path);
+        }
+
+        path.pop_back();
+    }
+
+    vector<vector<string>> findLadders(string beginWord,
+                                       string endWord,
+                                       vector<string>& wordList) {
+
+        unordered_set<string> st(wordList.begin(), wordList.end());
+
+        if (!st.count(endWord))
+            return {};
+
+        queue<string> q;
+
+        q.push(beginWord);
+        level[beginWord] = 0;
+
+        while (!q.empty()) {
+
+            string word = q.front();
+            q.pop();
+
+            int currLevel = level[word];
+
+            string temp = word;
+
+            for (int i = 0; i < temp.size(); i++) {
+
+                char original = temp[i];
+
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+
+                    if (ch == original)
+                        continue;
+
+                    temp[i] = ch;
+
+                    if (!st.count(temp))
+                        continue;
+
+                    if (!level.count(temp)) {
+
+                        level[temp] = currLevel + 1;
+                        q.push(temp);
+                        parent[temp].push_back(word);
+
+                    }
+                    else if (level[temp] == currLevel + 1) {
+
+                        parent[temp].push_back(word);
+                    }
+                }
+
+                temp[i] = original;
+            }
+        }
+
+        if (!level.count(endWord))
+            return {};
+
+        vector<string> path;
+
+        dfs(endWord, beginWord, path);
+
+        return ans;
+    }
+};
