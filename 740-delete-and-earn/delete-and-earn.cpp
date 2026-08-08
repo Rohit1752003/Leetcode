ee3bd2cf0220f076dd525e4090493b29a3c6d43c
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-08-08 11:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (19.78%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,40 +1,28 @@
 class Solution {
-    int solve(int i, vector<pair<int,int>> &v, vector<int> &dp) {
+    int solve(int i ,vector<pair<int,int>>& nums,  vector<int>& dp){
+        int n = nums.size();
+        if(i>=n)return 0 ;
+        if(dp[i] != -1)return dp[i];
+        int include = nums[i].second ;
+        if(i+1 < n && nums[i+1].first == nums[i].first +1)
+          include  +=   solve(i+2 , nums, dp);
 
-        if (i >= v.size())
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        int include = v[i].second;
-
-        // If next value is consecutive, skip it
-        if (i + 1 < v.size() && v[i + 1].first == v[i].first + 1)
-            include += solve(i + 2, v, dp);
-        else
-            include += solve(i + 1, v, dp);
-
-        int exclude = solve(i + 1, v, dp);
-
-        return dp[i] = max(include, exclude);
+       else
+            include += solve(i + 1, nums, dp);
+            
+            int exclude = solve(i+1 , nums, dp);
+            return dp[i] = max(include , exclude);
+        
     }
-
 public:
     int deleteAndEarn(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto it : nums)mp[it] += it;
 
-        map<int,int> mp;
-
-        for (int x : nums)
-            mp[x] += x;        // store total points, not frequency
-
-        vector<pair<int,int>> v;
-
-        for (auto &it : mp)
-            v.push_back(it);
-
-        vector<int> dp(v.size(), -1);
-
-        return solve(0, v, dp);
+        vector<pair<int,int>> arr;
+        for(auto it : mp) arr.push_back(it);
+        sort(arr.begin(), arr.end());
+        vector<int> dp(arr.size(), -1);
+        return solve(0,arr, dp);
     }
 };
