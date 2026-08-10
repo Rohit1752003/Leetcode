e3b6a1606a6b117a76cb2d2d4fa1c51c76b460f3
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-08-10 13:39:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (31.32%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int solve(int i,int j,vector<vector<int>>& dp){
+
+    if(i==0 && j==0) return 1;
+
+    if(i<0 || j<0) return 0;
+
+    if(dp[i][j] != -1)
+        return dp[i][j];
+
+    int down = solve(i-1,j,dp);
+
+    int right = solve(i,j-1,dp);
+
+    return dp[i][j]=down+right;
+}
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m , vector<int>(n,-1));
+        return solve(m-1,n-1, dp);
+    }
+};
