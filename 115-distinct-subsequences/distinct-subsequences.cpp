b38ee9e0ecbc81a3ef0136c13418040ea3ab58ad
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-08-22 11:14:27 +0530

    Time: 38 ms (23.97%) | Memory: 26.9 MB (72.99%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,16 +1,14 @@
 class Solution {
     int solve(int i , int j , string &s , string &t ,   vector<vector<int>> &dp){
-        if(j <0) return 1;
-        if(i < 0 )return 0;
+       if(j< 0 )return 1;
+       if( i < 0 ) return 0;
 
-        if(dp[i][j] != -1)return dp[i][j];
-        if(s[i] == t[j] ){
-            dp[i][j] = solve(i-1, j-1, s, t, dp) + solve(i-1, j , s,t, dp);
-        }
-        else 
-         dp[i][j] = solve( i-1, j , s, t, dp);
-         return dp[i][j]; 
-        
+       if(dp[i][j] != -1)return dp[i][j];
+       if(s[i] == t[j] ){
+        dp[i][j] = solve(i-1, j-1, s,t, dp) + solve(i-1, j , s, t, dp);
+       }
+       else dp[i][j] = solve(i-1, j , s, t, dp);
+       return dp[i][j];
        
     }
 public:
@@ -20,4 +18,4 @@ public:
           vector<vector<int>> dp (n , vector<int> (m , -1));
           return solve(n-1,m-1, s, t, dp);
     }
-};
+};  
