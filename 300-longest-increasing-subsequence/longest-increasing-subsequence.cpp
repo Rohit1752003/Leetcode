1a349ab0a9bce2f69b55f38a1430fd5c6342b541
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-08-31 12:27:10 +0530

    Time: 1 ms (81.37%) | Memory: 14 MB (87.30%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -34,17 +34,34 @@ int lengthOfLIS(vector<int>& nums) {
 
 
     // Tabulation 
-    int ans = INT_MIN;
-    vector<int> dp ( n+1 , 1);
-    for(int i = 0  ; i < n ; i++){
-        for(int j = 0 ; j < i ; j++){
-            if(nums[j] < nums[i]){
-                dp[i] = max(dp[i] , dp[j]+1);
-            }
-        }
-        ans = *max_element(dp.begin() , dp.end());
-    }
-    return ans;
+    // int ans = INT_MIN;
+    // vector<int> dp ( n+1 , 1);
+    // for(int i = 0  ; i < n ; i++){
+    //     for(int j = 0 ; j < i ; j++){
+    //         if(nums[j] < nums[i]){
+    //             dp[i] = max(dp[i] , dp[j]+1);
+    //         }
+    //     }
+      
+    // }
+    // return   *max_element(dp.begin() , dp.end());
+    
 
+    //  Approach 3 Binary Search
+    vector<int> temp;
+    temp.push_back(nums[0]);
+    int len =1; 
+    for(int i = 1 ; i < n ; i++){
+        if(nums[i] > temp.back()){
+            temp.push_back(nums[i]);
+            len++;
+        }
+        else{
+            int ind = lower_bound(temp.begin() , temp.end() , nums[i])- temp.begin();
+            temp[ind] = nums[i];
+
+        }
+    }
+    return len;
 }
 };
