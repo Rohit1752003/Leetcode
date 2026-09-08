745aab86ef709dbfde8a418126eff4b3599a75b4
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-09-08 12:00:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (64.73%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,12 @@
 class Solution {
     int cost(int i ,vector<int>& nums,vector<int>& dp){
-        int n = nums.size();
-         
-        if(i >= n)return 0;
-      
-        if(dp[i]!=-1)return dp[i];
-    
-           int   include = nums[i] + cost(i+2,nums,dp);
-             int  exclude= cost(i+1,nums,dp);
-
-            return dp[i]= max(include , exclude);
-       
+         int n = nums.size();
+        //  if(i == n-1 )return nums[n-1];
+         if(i > n-1)return 0;
+         if(dp[i] != -1)return dp[i];
+         int exclude = cost(i+1 ,nums , dp);
+         int include = nums[i] + cost(i+2, nums , dp);
+         return dp[i] = max(exclude , include);
         
     }
 public:
