ae9058818ab7f235a656075c808d7f5f4653a3b1
Rohit Wankhede <rohitwankhede1752003@gmail.com>
2026-09-22 11:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (94.27%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,20 +1,36 @@
 class Solution {
+   
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        if (!root) return ans;
+        // if (!root) return ans;
 
-        // Visit root first
-        ans.push_back(root->val);
+        // // Visit root first
+        // ans.push_back(root->val);
 
-        // Traverse left subtree
-        vector<int> left = preorderTraversal(root->left);
-        ans.insert(ans.end(), left.begin(), left.end());
+        // // Traverse left subtree
+        // vector<int> left = preorderTraversal(root->left);
+        // ans.insert(ans.end(), left.begin(), left.end());
 
-        // Traverse right subtree
-        vector<int> right = preorderTraversal(root->right);
-        ans.insert(ans.end(), right.begin(), right.end());
+        // // Traverse right subtree
+        // vector<int> right = preorderTraversal(root->right);
+        // ans.insert(ans.end(), right.begin(), right.end());
 
+        // return ans;
+
+
+         if(!root )return ans ;
+
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            if(node->right)st.push(node->right);
+             if(node->left)st.push(node->left);
+
+        }
         return ans;
     }
 };
